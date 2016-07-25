7cd271d7a509ed1d3c943aa79d9971ba971e1338
yevgeny hong <yev_science@naver.com>
2016-07-25 17:50:44 +0900

    Update key_event.py

--- key_event.py
+++ key_event.py
@@ -3,11 +3,14 @@ import serial
 user = ctypes.windll.user32
 
 ser = serial.Serial()
-ser.port = 'COM33'
-ser.baudrate = 9600
+ser.port = 'COM33' # put your arduino COM port
+ser.baudrate = 9600 # put your arduino baudrate
 ser.open()
 while (True):
     if(ser.inWaiting() > 0):
         HEX = ord(ser.read())
-        user.keybd_event(HEX,0,1,0)
-        user.keybd_event(HEX,0,2,0)
+        user.keybd_event(HEX,0,1,0) # press! 
+        user.keybd_event(HEX,0,2,0) # up!
+
+
+# use what do you want to do
